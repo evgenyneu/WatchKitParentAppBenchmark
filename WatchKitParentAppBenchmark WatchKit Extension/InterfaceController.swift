3ccd3b4362d3a7e437b837c9cb5ea524f58ee611
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-05-09 11:53:33 +1000

    Compute deviation

--- WatchKitParentAppBenchmark WatchKit Extension/InterfaceController.swift
+++ WatchKitParentAppBenchmark WatchKit Extension/InterfaceController.swift
@@ -1,31 +1,79 @@
-//
-//  InterfaceController.swift
-//  WatchKitParentAppBenchmark WatchKit Extension
-//
-//  Created by Evgenii Neumerzhitckii on 9/05/2015.
-//  Copyright (c) 2015 Evgenii Neumerzhitckii. All rights reserved.
-//
-
 import WatchKit
 import Foundation
 
 
 class InterfaceController: WKInterfaceController {
 
-    override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
-        
-        // Configure interface objects here.
+  @IBOutlet weak var label: WKInterfaceLabel!
+  @IBOutlet weak var labelPerformance: WKInterfaceLabel!
+  @IBOutlet weak var labelAverage: WKInterfaceLabel!
+  @IBOutlet weak var labelDeviation: WKInterfaceLabel!
+  
+  private var numberOfMeasuresAfterFirstOne: Double = -1
+  private var timingsSinceFirst = [Double]()
+  
+  override func awakeWithContext(context: AnyObject?) {
+    super.awakeWithContext(context)
+    
+    label.setText(" ")
+    labelPerformance.setText(" ")
+    labelAverage.setText(" ")
+    labelDeviation.setText(" ")
+  }
+  
+  override func willActivate() {
+    // This method is called when watch view controller is about to be visible to user
+    super.willActivate()
+  }
+  
+  override func didDeactivate() {
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
+  }
+  
+  private func getDataFromParentApp(onReply: ()->()) {
+    WKInterfaceController.openParentApplication([:]) { reply, error in
+      
+      if let reply = reply as? [String: String] {
+        self.label.setText(reply["hi"])
+      }
+      
+      onReply()
     }
+  }
+  
+  private func showTiming(timeElapsed: Double) {
+    saveTiming(timeElapsed)
+    let elapsedFormatted = String(format: "%.1f ms", timeElapsed)
+    
+    let average = Statistics.average(timingsSinceFirst)
+    let averageFormated = average == 0 ? " " : String(format: "Average: %.1f ms", average)
+    
+    let standardDeviation = Statistics.standardDeviation(timingsSinceFirst)
+    
+    let standardDeviationFormatted = standardDeviation == 0 ? " " :
+      String(format: "Deviation: %.1f ms", standardDeviation)
 
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
+    
+    labelPerformance.setText(elapsedFormatted)
+    labelAverage.setText(averageFormated)
+    labelDeviation.setText(standardDeviationFormatted)
+
+  }
+  
+  private func saveTiming(timeElapsed: Double) {
+    numberOfMeasuresAfterFirstOne += 1
+    
+    // don't count the first timing as it is much slower probably because... I have not idea why.
+    if numberOfMeasuresAfterFirstOne == 0 { return }
+    
+    timingsSinceFirst.append(timeElapsed)
+  }
+  
+  @IBAction func onGetTimeTapped() {
+    let tick = TickTock()
+    getDataFromParentApp { [weak self] in
+      self?.showTiming(tick.measure())
     }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-
+  }
 }
---
+++ WatchKitParentAppBenchmark WatchKit Extension/Utils/TickTock.swift
@@ -0,0 +1,38 @@
+//  
+//  Measures code execution time.
+//
+//  Usage:
+//
+//    let timer = iTickTock()
+//    ... code to measure execution time for
+//    timer.output()
+//
+//    Output: [TOCK] 10.2 ms
+//
+
+import Foundation
+
+public class TickTock {
+  var startTime:NSDate
+
+  public init() {
+    startTime = NSDate()
+  }
+
+  public func measure() -> Double {
+    return Double(Int(-startTime.timeIntervalSinceNow * 10000)) / 10
+  }
+
+  public func formatted() -> String {
+    let elapsedMs = measure()
+    return String(format: "%.1f", elapsedMs)
+  }
+
+  public func formattedWithMs() -> String {
+    return "[TOCK] \(formatted()) ms"
+  }
+
+  public func output() {
+    println(formattedWithMs())
+  }
+}
